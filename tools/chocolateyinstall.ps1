bf8a1a04fc6bcd37617cd43deb076a6f3ce0db21
Oliver Waddell <wadoli@users.noreply.github.com>
2024-05-02 20:08:59 +0200

    v1.16.7

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$url        = ''
-$url64      = 'https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup_1.14.3.exe'
+$url64      = 'https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup_1.16.7.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -14,7 +14,7 @@ $packageArgs = @{
   softwareName  = 'Proton Pass*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
   #checksum      = '{{Checksum}}'
   #checksumType  = '{{ChecksumType}}' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '2893CDA537C814017180BC627C052DD69FF5ECAE66BEB8600B707B244A1C025E'
+  checksum64    = '6ED86BF91D59F16D0F4F02CF8961CBCA461264160CB3F5CD326EDF71297CC948'
   checksumType64= 'sha256' #default is checksumType
 }
 
